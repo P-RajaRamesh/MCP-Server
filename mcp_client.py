fdce0a3fb7cd69afab0cd4320463cc6b3a069c90
Pericharla Raja Ramesh <87027830+P-RajaRamesh@users.noreply.github.com>
2025-06-01 21:18:08 +0530

    Update mcp_client.py

--- mcp_client.py
+++ mcp_client.py
@@ -2,27 +2,31 @@ import gradio as gr
 
 import os
 
-from smolagents import InferenceClientModel, CodeAgent#, ToolCollection
+from smolagents import InferenceClientModel, CodeAgent
 from smolagents.mcp_client import MCPClient
 
 
 try:
     mcp_client = MCPClient(
-        ## Try this working example on the hub:
-        # {"url": "https://abidlabs-mcp-tools.hf.space/gradio_api/mcp/sse"} # --server: https://huggingface.co/spaces/abidlabs/mcp-tools
-        {"url": "https://rajaramesh-mcp-sentiment.hf.space/gradio_api/mcp/sse",
-         "transport": "sse" # You can change to "streamable-http" when updating to version 1.20
-         } # --remote server: https://huggingface.co/spaces/rajaramesh/mcp-sentiment/
-        # {"url": "http://localhost:7860/gradio_api/mcp/sse"} # --local server
+        # #--remote server: https://huggingface.co/spaces/username/mcp-sentiment/
+        # {
+        #     "url": "https://username-mcp-sentiment.hf.space/gradio_api/mcp/sse",
+        #     "transport": "sse" # You can change to "streamable-http" when updating to version 1.20
+        # } 
+        {"url": "http://localhost:7860/gradio_api/mcp/sse"} # --local server
     )
     tools = mcp_client.get_tools()
     print(f"my tools: {[*tools]}")
 
-    model = InferenceClientModel(
-        model_id="deepseek-ai/DeepSeek-R1",
-        provider="together",
-        token=os.getenv('HF_TOKEN')
-    )
+    # For local server
+    model = InferenceClientModel()
+
+    # For Huggingface Space
+    # model = InferenceClientModel(
+    #     model_id="deepseek-ai/DeepSeek-R1",
+    #     provider="together",
+    #     token=os.getenv('HF_TOKEN')
+    # )
     
     agent = CodeAgent(tools=[*tools], model=model)
 
